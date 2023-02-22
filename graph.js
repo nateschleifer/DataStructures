629d36e819a5f4ced3de39485260d593cd4fd9b8
Nathan Schleifer <nateschleifer@hotmail.com>
2023-02-22 17:27:25 -0600

    Created graph data structure.

---
+++ graph.js
@@ -0,0 +1,48 @@
+// no hierarchy; directed vs undirected(bidirectional)
+// disconnected, vertices without edges, multiple edges from single node, cycle, self loop on a node, weighted edges
+//adjacency matrix vs adjacency list; adjacency list more efficient O(n), can also store weights
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) { // if this key does not exist
+            this.adjacencyList[vertex] = new Set(); // add this key with value of set
+        }
+    }
+
+    addEdge(vertex1, vertex2) {
+        if(!this.adjacencyList[vertex1]) { //if this vertex does not exist as a key
+            this.addVertex(vertex1); // call addVertex for this key
+        }
+        if(!this.adjacencyList[vertex2]) {
+            this.addVertex(vertex2);
+        }
+        this.adjacencyList[vertex1].add(vertex2); // since undirected(bidirectional)
+        this.adjacencyList[vertex2].add(vertex1); // add vertexes to each others sets as values
+    }
+
+    hasEdge(vertex1, vertex2) {
+        return (
+            this.adjacencyList[vertex1].has(vertex2) &&
+            this.adjacencyList[vertex2].has(vertex1)
+        )
+    }
+
+    display() {
+        for(let vertex in this.adjacencyList) {
+            console.log(vertex + " -> " + [...this.adjacencyList[vertex]]);
+        }
+    }
+}
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+
+graph.display();
+console.log(graph.hasEdge("A", "C"));
